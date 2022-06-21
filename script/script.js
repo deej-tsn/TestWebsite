0d9cc421d0b40bda3376d063b85ca3646b947d4a
Macbook <macbook@Macbooks-MBP-2.broadband>
2022-06-22 00:22:45 +0100

    Functional but needs more ideas to make use of binary option

--- script/script.js
+++ script/script.js
@@ -4,10 +4,20 @@ let colour_two_object;
 function changeColour1(){
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     colour_one_object.style.backgroundColor = '#' + randomColor;
+    colour_one_object.innerHTML = '#' + randomColor.toUpperCase();
 };
+
+function changeColour2(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    colour_two_object.style.backgroundColor = '#' + randomColor;
+    colour_two_object.innerHTML = '#' + randomColor.toUpperCase();
+};
+
 
 window.onload = function(){
     colour_one_object = document.getElementById("colour1");
     colour_two_object = document.getElementById("colour2");
     colour_one_object.addEventListener("click", changeColour1);
+
+    colour_two_object.addEventListener("click", changeColour2);
 };
